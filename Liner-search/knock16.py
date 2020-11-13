ae2a10ccda7090e49c590a3a28b5ac56e37512e2
aktsan <aktbox6@yahoo.co.jp>
2020-11-13 23:45:14 +0900

    added

---
+++ Liner-search/knock16.py
@@ -0,0 +1,17 @@
+from itertools import permutations 
+n = int(input())
+P = tuple(map(int,input().split()))
+Q = tuple(map(int,input().split()))
+#print(P,Q)
+li = list(permutations(range(1,n+1)))
+p_index = 0
+q_index = 0
+for i in range(len(li)):
+    if li[i] == P and li[i] == Q:
+        print(0)
+        exit()
+    if li[i] == P:
+        p_index = i+1
+    elif li[i] == Q:
+        q_index = i+1
+print(abs(p_index-q_index))
