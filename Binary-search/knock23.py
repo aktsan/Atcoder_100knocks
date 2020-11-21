fc07003e13184718806f2faa702475c817a27453
aktsan <aktbox6@yahoo.co.jp>
2020-11-21 10:06:42 +0900

    re add

--- Binary-search/knock23.py
+++ Binary-search/knock23.py
@@ -1,3 +1,12 @@
-N,M = map(int,input().split())
-li = [int(input()) for _ in range(N)]
-
+import bisect
+N, M, *P = [int(_) for _ in open(0).read().split()]
+dp = [set() for _ in range(3)]
+dp[0].add(0)
+for p in P:
+    for i in range(2):
+        for x in dp[i]:
+            if x + p <= M:
+                dp[i + 1].add(x + p)
+li = sorted(dp[0] | dp[1] | dp[2])
+ans = max(x + li[min(len(li), bisect.bisect_right(li, M - x)) - 1] for x in li)
+print(ans)
